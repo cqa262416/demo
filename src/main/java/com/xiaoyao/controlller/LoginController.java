1e3d06cd33794db2984e9f7ff6879cf0f1a01f11
cqa262416 <cqa262416@gmail.com>
2017-02-17 11:08:31 +0800

    /user/login

--- src/main/java/com/xiaoyao/controlller/LoginController.java
+++ src/main/java/com/xiaoyao/controlller/LoginController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class LoginController {
     //登录
-    @RequestMapping(value = "/")
+    @RequestMapping(value = "/user/login")
     public String login(){
         return "login.html";
     }
