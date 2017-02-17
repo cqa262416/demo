94f2c19db2cfc6f66f73f68b17605ae4b13fc6ed
cqa262416 <cqa262416@gmail.com>
2017-02-17 10:33:14 +0800

    new a project

---
+++ src/main/java/com/xiaoyao/controlller/LoginController.java
@@ -0,0 +1,16 @@
+package com.xiaoyao.controlller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by xiaoyao on 17-2-17.
+ *
+ */
+@Controller
+public class LoginController {
+    @RequestMapping(value = "/")
+    public String login(){
+        return "login.html";
+    }
+}
